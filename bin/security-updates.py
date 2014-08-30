3f627dd37f3b207b66db28aa9d781ef138dc64ce
nadyawilliams <nadya@sdsc.edu>
2014-08-29 19:15:40 -0700

    fix broken lines resulting form subprocess buffering add comments

--- bin/security-updates.py
+++ bin/security-updates.py
@@ -1,4 +1,4 @@
-#!/opt/rocks/bin/python
+#!/opt/rocks/bin/python 
 #
 # @Copyright@
 # 
@@ -69,14 +69,12 @@ import rocks.app
 class App(rocks.app.Application):
 	def __init__(self, argv):
 		rocks.app.Application.__init__(self, argv)
-
-		self.getopt.l.extend([
-			('list=', ''),
-                        ])
+		self.getopt.l.extend([ ('list=', ''), ])
 		
 		self.setDefaults()
 
 	def setDefaults(self):
+		""" set default variables. Most are related to roll dir structure"""
 		self.rpms = []
 		self.downloads = []
 
@@ -101,6 +99,7 @@ class App(rocks.app.Application):
 		self.setRolldirs()
 
 	def writeHeader(self):
+		""" header of nodes xml file """
 		txt = '<?xml version="1.0" standalone="no"?>\n'
 		txt += '<kickstart>\n'
 		txt += '<description>\n'
@@ -110,31 +109,41 @@ class App(rocks.app.Application):
 		return txt
 
 	def writeFooter(self):
+		""" footer of nodes xml file """
 		txt =  '</kickstart>\n'
 		return txt
 
-	def writeXML(self):
+	def writeXml(self):
+		""" write packages for node xml file """
 		content = self.writeHeader()
 		for rpm in self.rpms:
 			content += '<package>%s</package>\n' % rpm
 		content += self.writeFooter()
+		
+		self.writeFile(self.nodexml, content)
 
-		fout = open(self.nodexml , 'w')
-		fout.write(content)
-		fout.close()
+	def writeFile(self, fname, content):
+		"""write content into a file  fname"""
+		try: 
+			fout = open(fname , 'w')
+			fout.write(content)
+		except IOError:
+			print "Error writing %s file" % fname
+		else:
+			fout.close()
+			print "Wrote %s" % fname
 
 	def writeScript(self):
+		""" write script for rpms download"""
 		#if os.path.isfile(self.download_script):
 		#	os.remove(self.download_script)
 		
 		content = "#!/bin/bash\n\n"
 		content += "\n".join(self.downloads)
-
-		fout = open(self.download_script, 'w')
-		fout.write(content)
-		fout.close()
+		self.writeFile(self.download_script, content)
 
 	def checkSecPlugin(self):
+		""" install and enable  yum security plugin"""
 		# check if yum security plugin is installed
 		cmd = "/bin/rpm -qa | /bin/grep %s" % self.plugin
 		info, err = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
@@ -158,7 +167,7 @@ class App(rocks.app.Application):
 			self.enableYumPlugin()
 
 	def installYumPlugin(self):
-		# install yum security plugin
+		""" install yum security plugin """
 		print "Installing %s" %self.plugin
 		cmd = "/usr/bin/yum --enablerepo=base install %s" % self.plugin
 		info, err = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
@@ -166,7 +175,7 @@ class App(rocks.app.Application):
 			print "Error in installYumPlugin(): %s" % err
 
 	def enableYumPlugin(self):
-		# enable yum security plugin
+		""" enable yum security plugin """
 		print "Enabling %s" %self.plugin
 		cmd = "/bin/echo %s >> /etc/yum.conf" % self.plugin_enabled
 		info, err = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
@@ -174,6 +183,7 @@ class App(rocks.app.Application):
 			print "Error in enableYumPlugin(): %s" % err
 			
 	def getErrata(self):
+		""" download security errata xml file """
 		self.createDir(self.updatesdir)
 		print "Fetching errata.latest.xml"
 		info, err = subprocess.Popen(self.errata_wget, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
@@ -181,23 +191,26 @@ class App(rocks.app.Application):
 			print "Error in getErrata(): %s" % err
 
 	def generateUpdateInfo(self):
+		""" run script to generate updated security info""" 
 		cmd = "%s %s" % (self.update_script, self.errata)
 		info, err = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
 
 	def getLocalRepos(self):
-		# create list of repos to look for updates 
+		""" create list of host yum repos to look for updates """
 		self.arch = platform.machine()
 		reposDir = glob.glob("%s/%s/*/" % (self.yumrepos, self.arch) )[0]
 		self.reposlist = os.listdir(reposDir)
 		self.reposlist = [ x for x in self.reposlist if x.find("Rocks") < 0 ] 
 
 	def setRolldirs(self):
+		""" create dir structure under roll's RPMS/"""
 		for name in [self.arch, "noarch"]:	
 			archdir = "%s/RPMS/%s" % (self.rolldir, name)	
 			self.createDir(archdir)
 		self.createDir("%s/nodes" % self.rolldir)
 						
 	def createDir(self, path):
+		""" create directory """
 		if  not os.path.exists(path):
 			try:
 				os.makedirs(path)
@@ -206,23 +219,22 @@ class App(rocks.app.Application):
 					raise
 
 	def getSecurityUpdates(self):
+		""" read created  updates info and creates security updates for chosen releases"""
 		reposString = ",".join(self.reposlist)
 		for release in self.releases:
 			self.modifyRepodata(release)
 			content = self.listUpdates(reposString)
 			fname = "%s/%s.out" % (self.updatesdir, release)
-			try:
-				fout = open(fname , 'w')
-				fout.write(content)
-				fout.close()
-			except IOError :
-				continue
+			self.writeFile(fname, content)
 			self.parsePackageNames(content)
 
 
 	def parsePackageNames(self, content):
+		""" parse text in content to find info about security packags """
+		# split text str into lines
 		lines = content.split("\n")
 		
+		# collect lines with packages info
 		count = 0
 		startline = 0
 		endline = len(lines)
@@ -233,18 +245,40 @@ class App(rocks.app.Application):
 				endline = count
 			count += 1
 
-		lines = lines[startline:endline]
+		pkglines = lines[startline:endline]
+
+		# fix split lines if present
+		num = len(pkglines)
+		lines = []
+		count = 0
+		while count < num:
+			line = pkglines[count]
+			if len(line) < 1:
+				count += 1
+				continue
+			if len(line.split()) < 3:
+				lines.append(line[:-1] + pkglines[count+1])
+				count = count + 2
+			else:
+				lines.append(line)
+				count += 1
+
+		# parse package lines to collect rpm names and downloads commands
 		for l in lines:
-			if len(l) < 1: continue
 			rpmarch,version,repo = l.split()
 			rpm,arch = rpmarch.split(".")
 			if rpm in self.rpms: 
 				continue
+			# collect package names
 			self.rpms.append(rpm)
-			self.downloads.append("/usr/bin/yumdownloader --resolve --destdir %s/RPMS/%s --enablerepo %s %s" 
-						% (self.rolldir, arch, repo, rpmarch))
+
+			# create yum download commands
+			cmd = "/usr/bin/yumdownloader --resolve --destdir %s/RPMS/%s --enablerepo %s %s" % \
+				(self.rolldir, arch, repo, rpmarch)
+			self.downloads.append(cmd)
 		
 	def testFunc(self):
+		""" reads packages updates info  and creates a list of rpms and downloads commands"""
 		reposString = ",".join(self.reposlist)
 		for release in self.releases:
 			fname = "%s/%s.out" % (self.updatesdir, release)
@@ -256,33 +290,33 @@ class App(rocks.app.Application):
 				continue 
 			self.parsePackageNames(content)
 
+		for rpm in self.rpms :
+			print rpm
+		for cmd  in self.downloads :
+			print cmd
+
 	def modifyRepodata(self, release):
+		""" modify  yum repo data based on updateinfo.xml for a given release """
 		cmd = "/usr/bin/modifyrepo %s/%s/updateinfo.xml %s" % (self.updatesdir, release, self.repodata)
+		print "Executing: ", cmd
 		info, err = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
 		cmd = "/usr/bin/yum  clean all"
+		print "Executing: ", cmd
 		info, err = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
 
 	def listUpdates(self, repolist):
+		""" check for security updates """
 		cmd = "/usr/bin/yum --security --enablerepo=%s check-update" % repolist
+		print "Executing: ", cmd
 		info, err = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
 		return info
 
-	def getSuffix(self,filename):
-		suffix_dict = {'py':'/opt/rocks/bin/python','csh':'/bin/tcsh','pl':'/usr/bin/perl'}
-		suffix = filename.split('.')[-1]
-		try:
-			interpreter = suffix_dict[suffix]
-		except KeyError:
-			interpreter = ""
-
-		return interpreter
-		
 	def run(self):
 		self.checkSecPlugin()
 		self.getErrata()
 		self.generateUpdateInfo()
 		self.getSecurityUpdates()
-		self.writeXML()
+		self.writeXml()
 		self.writeScript()
 
 	def runTest(self):
